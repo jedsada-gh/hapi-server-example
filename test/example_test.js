97a43a013e7369464dc9c449f33e52bf866fd6f4
Jedsada Tiwongvorakul <jt@20scoops.com>
2017-07-26 18:12:44 +0700

    improvment code.

--- test/example_test.js
+++ test/example_test.js
@@ -1,3 +1,6 @@
-const test = require('unit.js');
-
-test.assert(4==4)
+var assert = require('assert');
+describe('validate number', function () {
+    it('should be number 5 when the value is 5', function () {
+        assert.equal(5, 5);
+    });
+});
