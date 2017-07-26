bb013b0a8282deb0e82e31264594a07ce3e0cb01
Jedsada Tiwongvorakul <jt@20scoops.com>
2017-07-26 14:58:59 +0700

    improvment code.

--- src/user-info-model.js
+++ src/user-info-model.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserInfoSchema = new Schema({
-	name: String
+	username: String,
+	first_name: String,
+	last_name: String,
+	age: Number
 });
 
 module.exports = mongoose.model('User', UserInfoSchema);
